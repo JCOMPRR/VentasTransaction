9e0f0497afa084a001e0935b2671ed6831130829
JCOMPRR <94765491+JCOMPRR@users.noreply.github.com>
2023-04-22 17:31:19 -0700

    Agregar archivos de proyecto.

---
+++ AccesoDatos/Instrucciones.cs
@@ -0,0 +1,31 @@
+﻿//Crear un repositorio en GitHub para ir subiendo sus cambios
+
+
+//En el archivo Form1.cs, está el método GuardarVenta, con el que estamos ejecutando varios querys a base de datos
+//en una transaction, este código se deberá separar en varios métodos y ubicarlo en el proyecto de AccesoDatos,
+//es muy importante que se mantenga la transaction.
+
+
+//Crear los métodos de crear, actualizar, editar, eliminar de productos y clientes.
+
+
+//Se debe considerar que, al dar de alta los productos, debe crearse en existencias un registro con el id del
+//producto con existencia en cero.
+
+
+//Crear método de actualización de existencias.
+
+
+//Los folios no deberán ser modificados por el sistema.
+
+
+//Se deberán crear las pantallas del sistema, estas no deben acceder a base de datos, todo el acceso a base de datos
+//será por medio del proyecto AccesoDatos.
+
+
+//En caso de no tener la base de datos creada en clase, dentro del proyecto de AccesoDatos, existe una carpeta
+//llamada ScriptDB, en esta están dos archivos con extensión sql, VentasTransactionDB.sql este es un script que
+//genera la base de datos y VentasTransactionDB_Seed.sql agrega registros mínimos necesarios para que hagan sus
+//pruebas.
+
+//Se entregará la url para el acceso a su repositorio y archivo zip descargado del mismo
---
+++ AccesoDatos/Producto.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AccesoDatos
+{
+    public class Producto
+    {
+        public int Id { get; set; }
+        public string Descripcion { get; set; }
+        public decimal PrecioUnitario { get; set; }
+
+
+        //Aqui se agrega en Existencias
+        public void AgregarProducto(Producto producto)
+        {
+            try
+            {
+                string query = "INSERT INTO Existencias" +
+                    "(Descripcion,PrecioUnitario) " +
+                    "VALUES" +
+                    "(@Descripcion,@PrecioUnitario)";
+
+                using (SqlConnection con = new SqlConnection(query))
+                {
+                    SqlTransaction transaction = con.BeginTransaction();
+                    con.Open();
+
+                    using (SqlCommand cmd = new SqlCommand(query, con))
+                    {
+                        cmd.CommandType = CommandType.Text;
+                        cmd.Transaction = transaction;
+
+                        cmd.Parameters.AddWithValue("@Descripcion", producto.Descripcion);
+                        cmd.Parameters.AddWithValue("@PrecioUnitario", producto.PrecioUnitario);
+
+                        cmd.ExecuteNonQuery();
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+
+                throw new Exception(ex.Message);
+            }
+        }
+
+        //Actualiza el producto seleccionado mediante la Id
+        public void ActualizarProducto(Producto producto)
+        {
+            try
+            {
+                string query = "UPDATE Existencias SET Descripcion = " +
+                    "@Descripcion, PrecioUnitario = " +
+                    "@PrecioUnitario WHERE Id = @Id"; //Aqui especifica la Id que cambiara
+
+                using (SqlConnection con = new SqlConnection(query))
+                {
+                    SqlTransaction transaction = con.BeginTransaction();
+                    con.Open();
+
+                    using (SqlCommand cmd = new SqlCommand(query, con))
+                    {
+                        cmd.CommandType = CommandType.Text;
+                        cmd.Transaction = transaction;
+
+                        cmd.Parameters.AddWithValue("@Descripcion", producto.Descripcion);
+                        cmd.Parameters.AddWithValue("@PrecioUnitario", producto.PrecioUnitario);
+                        cmd.Parameters.AddWithValue("@Id", producto.Id);
+
+                        cmd.ExecuteNonQuery();
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+
+                throw new Exception(ex.Message);
+            }
+        }
+
+        //Aqui elimina el producto que seleccionemos 
+        public void EliminarProducto(int id) //Pongo el Id ya que se borrara mediante ella
+        {
+            try
+            {
+                string query = "DELETE FROM Existencias where Id = @Id";
+
+                using (SqlConnection con = new SqlConnection(query))
+                {
+                    SqlTransaction transaction = con.BeginTransaction();
+                    con.Open();
+
+                    using (SqlCommand cmd = new SqlCommand(query, con))
+                    {
+                        cmd.CommandType = CommandType.Text;
+                        cmd.Transaction = transaction;
+
+                        cmd.Parameters.AddWithValue("@Id", id);
+
+                        cmd.ExecuteNonQuery();
+                    }
+                }
+            }
+
+            catch (Exception ex)
+            {
+
+                throw new Exception(ex.Message);
+            }
+        }
+
+        public Producto SeleccionarProducto (int id)
+        {
+            try
+            {
+                string query = "SELECT Id, Descripcion, PrecioUnitario FROM Existencias WHERE Id = @Id";
+
+                using (SqlConnection con = new SqlConnection(query))
+                {
+                    con.Open();
+
+                    using (SqlCommand cmd = new SqlCommand(query, con))
+                    {
+                        cmd.CommandType = CommandType.Text;
+
+                        cmd.Parameters.AddWithValue("@Id", id);
+
+                        using (SqlDataReader reader = cmd.ExecuteReader())
+                        {
+                            if (reader.HasRows)
+                            {
+                                while (reader.Read())
+                                {
+                                    Producto producto = new Producto();
+
+                                    producto.Id = reader.GetInt32(0);
+                                    producto.Descripcion = reader.GetString(1);
+                                    producto.PrecioUnitario = reader.GetDecimal(2);
+
+                                    return producto;
+                                }
+                            }
+                        }
+                    }
+                }
+                return null;
+//No entiendo del todo, pero al agregar esto ya no da error en la Ln 117 "SeleccionarProducto" (Me spoileo el codigo)
+
+            }
+            catch (Exception ex)
+            {
+
+                throw new Exception(ex.Message);
+            }
+        }
+
+    }
+}
